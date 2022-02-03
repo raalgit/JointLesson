e58365821cd692a35a97dae1ee31f547646d652d
Raal <ivan.pyzhyanov.90@mail.ru>
2022-02-03 20:37:53 +0300

    init MVVM template with base components and classes

--- Terminal/JointLessonTerminal/MVVM/View/AuthWindow.xaml.cs
+++ Terminal/JointLessonTerminal/MVVM/View/AuthWindow.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace JointLessonTerminal
+namespace JointLessonTerminal.MVVM.View
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AuthWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : UserControl
     {
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
         }
---
+++ Terminal/JointLessonTerminal/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using JointLessonTerminal.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JointLessonTerminal.MVVM.ViewModel
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public AuthWindowViewModel AuthVM { get; set; }
+        public CourseListViewModel CourseVM { get; set; }
+
+        public RelayCommand AuthOpenCommand { get; set; }
+        public RelayCommand CourseOpenCommand { get; set; }
+
+
+        private object _currentView;
+        public object CurrentView { 
+            get {
+                return _currentView;
+            }
+            set
+            {
+                _currentView = value;
+                OnPropsChanged();
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            AuthVM = new AuthWindowViewModel();
+            CourseVM = new CourseListViewModel();
+
+            CurrentView = AuthVM;
+
+            AuthOpenCommand = new RelayCommand(x => CurrentView = AuthVM);
+            CourseOpenCommand = new RelayCommand(x => CurrentView = CourseVM);
+        }
+
+    }
+}
