c06de911ddd171ecaa7f730f577e6268bab7db06
Raal <ivan.pyzhyanov.90@mail.ru>
2022-03-14 22:56:56 +0300

    design revision

--- Terminal/JointLessonTerminal/Core/RemoteTerminalServer/RdpSessionServer.cs
+++ Terminal/JointLessonTerminal/Core/RemoteTerminalServer/RdpSessionServer.cs
@@ -65,7 +65,7 @@ namespace JointLessonTerminal.Core.RemoteTerminalServer
 
         public string CreateInvitation(string groupName, string passowrd)
         {
-            var invitation = _rdpSession.Invitations.CreateInvitation(null, groupName, passowrd, 1);
+            var invitation = _rdpSession.Invitations.CreateInvitation(null, groupName, passowrd, 2);
             return invitation.ConnectionString;
         }
 
