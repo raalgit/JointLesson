fff3fcaa7459281da201f857dc80307a809cd7c0
Raal <ivan.pyzhyanov.90@mail.ru>
2022-03-15 22:43:30 +0300

    window state

--- Terminal/JointLessonTerminal/MVVM/View/MainWindow.xaml.cs
+++ Terminal/JointLessonTerminal/MVVM/View/MainWindow.xaml.cs
@@ -20,15 +20,63 @@ namespace JointLessonTerminal.MVVM.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ImageSource maximizeBtnImage { get; set; }
+        public ImageSource minimizeBtnImage { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            maximizeBtnImage = new BitmapImage(new Uri("../../Images/max-btn.png", UriKind.Relative));
+            minimizeBtnImage = new BitmapImage(new Uri("../../Images/min-btn.png", UriKind.Relative));
+            WinStateBtnImage.Source = minimizeBtnImage;
+            mainWin.StateChanged += MainWin_StateChanged;
+            mainWin.MouseDoubleClick += MainWin_MouseDoubleClick;
+        }
+
+        private void MainWin_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowState == WindowState.Normal || WindowState == WindowState.Minimized)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void MainWin_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Normal || WindowState == WindowState.Minimized)
+            {
+                WinStateBtnImage.Source = maximizeBtnImage;
+            }
+            else
+            {
+                WinStateBtnImage.Source = minimizeBtnImage;
+            }
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
+        }
+
+        private void maximize_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal || WindowState == WindowState.Minimized) {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
