932e2ae91228e1a8c6c4428e906fe30a5dd10c00
Raal <ivan.pyzhyanov.90@mail.ru>
2022-03-21 15:56:37 +0300

    online lesson managed panels

--- Terminal/JointLessonTerminal/MVVM/View/LessonWindow.xaml.cs
+++ Terminal/JointLessonTerminal/MVVM/View/LessonWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AvalonDock.Layout.Serialization;
+using AvalonDock.Layout;
+using AvalonDock;
+using System.Diagnostics;
 
 namespace JointLessonTerminal.MVVM.View
 {
